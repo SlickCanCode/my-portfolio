f643e8077246ab3b74b6aa9e0d41c7ca0d2ecb31
Adelanwa Oreofe <emmanueldamilare121@gmail.com>
2025-06-08 22:24:11 +0100

    first commit

---
+++ flask-backend/agent.py
@@ -0,0 +1,88 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from tools import faq_tool
+
+load_dotenv()
+
+class ChatbotResponse(BaseModel):
+    response: str
+    tools_used: list[str]
+
+    def to_dict(self):
+        return {
+            "response": self.response,
+            "tools_used": self.tools_used
+        }
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+parser = PydanticOutputParser(pydantic_object=ChatbotResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system", 
+            """
+            You are my helpful assistant in my portfolio website that will answer questions about me and my services. 
+            Answer the user query in a natural, human-like friendly way and use necessary tools.
+            If the question is unrelated, politely guide the user back to relevant topics.
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}")
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+tools = [faq_tool]
+
+# Add memory
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+# Use memory in the agent executor
+def invoke_agent(query: str, chat_history: list[dict] = None):
+    # Create new memory buffer for this session
+    memory = ConversationBufferMemory(
+        memory_key="chat_history", 
+        return_messages=True
+    )
+
+    # If browser sends chat history for session, load it
+    if chat_history:
+        for msg in chat_history:
+            memory.chat_memory.add_message(msg)
+
+    agent = create_tool_calling_agent(
+        llm=llm,
+        prompt=prompt,
+        tools=tools
+    )
+
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        memory=memory,
+        verbose=True
+    )
+
+    raw_response = agent_executor.invoke({"query": query}) 
+
+    try:
+        structured_response = parser.parse(raw_response.get("output"))
+        return structured_response.to_dict()
+    except Exception as e:
+        return {
+            "error": str(e),
+            "raw_response": raw_response
+        }
+
+
+
+
+
---
+++ flask-backend/main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from agent import invoke_agent
+from contact import send_email as contact_me
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    data = request.get_json()
+    if not data or 'message' not in data:
+        return jsonify({'error': 'No message provided'}), 400
+    
+    user_message = data.get('message')
+    
+    # process the message and generate a response.
+    return jsonify(invoke_agent(user_message)), 200
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    data = request.get_json()
+    
+    name = data.get('name')
+    email = data.get('email')
+    message = data.get('message')
+
+    response = contact_me(name, email, message)
+
+    if "error" in response:
+        return jsonify({"error": "An Error Occured, Pls contact me directly via email"}), 500
+    else:
+        return jsonify({"success": "message sent"}),200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+# This will run the Flask app on all interfaces at port 5000.
